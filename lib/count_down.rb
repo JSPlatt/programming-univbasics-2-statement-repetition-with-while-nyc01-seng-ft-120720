b5b5aceb934e873c8b72768721742f15997519b8
Jeffrey Platt <jsplatt@gmail.com>
2020-11-19 18:27:36 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,9 @@
-# Write your code here
+magic_exit_number = 0 # Write your code here
+count = 10
+while count > 0 do
+  break if count == magic_exit_number
+  puts "#{count}"
+  count = count - 1
+while count > 0 && count !=magic_exit_number do
+  puts "Happy New Year!"
+end
